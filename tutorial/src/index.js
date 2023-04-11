3fc4e6361732fc407f91c51afadaf2bd4a33f5fa
TwilightLogic <twilightlogic@gmail.com>
2023-04-11 09:56:14 +0800

    update the notes in readme.md

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,26 +4,12 @@ import ReactDom from 'react-dom';
 // Capitalize the name of a function stands for a COMPONENT (Must capitalize the first letter!)
 function Greeting() {
   // JSX
-  // ⬇️ It is actually a 'function' (See GreetingFun())
   return (
     <div>
       <h1>Hello the fucking world!</h1>
     </div>
   );
 }
-
-// const GreetingFun = () => {
-//   return React.createElement('h1', {}, 'Hello world!');
-// };
-
-// With multiple tags
-// const GreetingMultiFun = () => {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, 'Hello World')
-//   );
-// };
 
 // 1. It is looking for what we're going to render. (of course the Greeting!)
 // 2. Where we want to render it? (the id 'root' in index.html)
