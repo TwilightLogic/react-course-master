f1e5809ee65c3bba96fcce00a0a0b0e5e3a52b26
TwilightLogic <twilightlogic@gmail.com>
2023-04-11 09:06:53 +0800

    I made my first component Greeting!

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Capitalize the name of a function stands for a COMPONENT
+function Greeting() {
+  // JSX
+  return <h1>Hello the fucking world!</h1>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 1. It is looking for what we're going to render. (of course the Greeting!)
+// 2. Where we want to render it? (the id 'root' in index.html)
+ReactDom.render(<Greeting />, document.getElementById('root'));
